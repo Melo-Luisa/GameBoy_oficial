9312b86b6aada81152030523b70d5e8753f4e360
João Ferreira <100808096+JoaoFerreira123@users.noreply.github.com>
2024-03-29 19:05:16 -0300

    create inputs.h

--- src/examples/teste_ESPNOW/main.cpp
+++ src/examples/teste_ESPNOW/main.cpp
@@ -1,67 +1,9 @@
-
-
+#include <Arduino.h>
 #include <esp_now.h>
 #include <WiFi.h>
 
-//PROTOTIPO FUNCÕES
+#include "communication.h"
 
-void getReadings();
-void updateDisplay();
-
-
-// REPLACE WITH THE MAC Address of your receiver 
-//uint8_t broadcastAddress[] = {0x78, 0x21, 0x84, 0xDF, 0x1C, 0x58}; //ESP VELHO
-uint8_t broadcastAddress[] = {0x54, 0x43, 0xB2, 0x46, 0x97, 0x0C}; //ESP NOVO
-//uint8_t broadcastAddress[] = {0x40, 0x22, 0xD8, 0x03, 0x81, 0xF0}; //ESP THETA
-
-
-// Define variables to store BME280 readings to be sent
-float toque;
-
-
-// Define variables to store incoming readings
-float incomingTouch;
-
-
-// Variable to store if sending data was successful
-String success;
-
-//Structure example to send data
-//Must match the receiver structure
-typedef struct struct_message {
-    float toque;
-
-} struct_message;
-
-// Create a struct_message called BME280Readings to hold sensor readings
-struct_message TOUCH_READING;
-
-// Create a struct_message to hold incoming sensor readings
-struct_message incomingReadings;
-
-esp_now_peer_info_t peerInfo;
-
-// Callback when data is sent
-void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
-  Serial.print("\r\nLast Packet Send Status:\t");
-  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
-  if (status ==0){
-    success = "Delivery Success :)";
-  }
-  else{
-    success = "Delivery Fail :(";
-  }
-}
-
-// Callback when data is received
-void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
-  memcpy(&incomingReadings, incomingData, sizeof(incomingReadings));
-  Serial.print("Bytes received: ");
-  Serial.println(len);
-  incomingTouch = incomingReadings.toque;
-
-}
- 
 void setup() {
   // Init Serial Monitor
   Serial.begin(115200);
@@ -111,25 +53,4 @@ void loop() {
     Serial.println("Error sending the data");
   }
   updateDisplay();
-  delay(0);
-}
-void getReadings(){
-  //COLOCAR UM GERADOR DE ALEATORIO AQUI
-  toque = touchRead(4);
-
-}
-
-void updateDisplay(){
-  
-  // Display Readings in Serial Monitor
-  Serial.println("INCOMING READINGS");
-  Serial.print("Touch: ");
-  Serial.print(incomingReadings.toque);
-  Serial.println();
-
-  if(incomingReadings.toque < 50){
-    digitalWrite(2, HIGH); 
-  }else{
-    digitalWrite(2, LOW);
-  }
 }
---
+++ src/main/inputs.h
@@ -0,0 +1,7 @@
+/*
+Criar um .h para a leitura e tratamento dos botões e joystick, pra não ficar fazendo leitura analógica e map e etc no meio do código.
+
+P/ o Joy, tanto função pra retornar valor com ele voltando sozinho, quanto pra incrementar
+
+e ETC
+*/
