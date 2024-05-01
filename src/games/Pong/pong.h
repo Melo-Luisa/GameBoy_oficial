58cce66ccfb948fe49eb54d321b3add49add1d29
Luisa <luhcotmel@gmail.com>
2024-05-01 08:36:52 -0300

    n tenho certeza o que foi alterado

--- src/games/Pong/pong.h
+++ src/games/Pong/pong.h
@@ -8,38 +8,38 @@
 #define EIXO_Y  32
 
 
-TFT_eSPI d = TFT_eSPI();  //init display
+// TFT_eSPI d = TFT_eSPI();  //init display
 
-TFT_eSprite ball = TFT_eSprite(&d);
-TFT_eSprite barra1 = TFT_eSprite(&d);
-TFT_eSprite barra2 = TFT_eSprite(&d);
-TFT_eSprite placar = TFT_eSprite(&d);
+// TFT_eSprite ball = TFT_eSprite(&d);
+// TFT_eSprite barra1 = TFT_eSprite(&d);
+// TFT_eSprite barra2 = TFT_eSprite(&d);
+// TFT_eSprite placar = TFT_eSprite(&d);
 
-//Ball Settings
-int circleRadius =10; 
-int vx = 10, vy = 10; //velocidade da bolinha
-int x = 100, y = 50;
+// //Ball Settings
+// int circleRadius =10; 
+// int vx = 10, vy = 10; //velocidade da bolinha
+// int x = 100, y = 50;
 
-int square_Width = 10;
-int square_Height = 60;
+// int square_Width = 10;
+// int square_Height = 60;
 
-//Barra joystick
-int coordX_B1 = 15;
-int coordY_B1 = 100;
+// //Barra joystick
+// int coordX_B1 = 15;
+// int coordY_B1 = 100;
 
-int coordY_B1_atual = 100;
-int coordY_B1_antiga = 100;
+// int coordY_B1_atual = 100;
+// int coordY_B1_antiga = 100;
 
 
-//Barra botoes
-int coordY_B2 = 100; //eh y
+// //Barra botoes
+// int coordY_B2 = 100; //eh y
 
-int botao_azul = 34; //azul
-int botao_amarelo = 35; // amarelo
+// int botao_azul = 34; //azul
+// int botao_amarelo = 35; // amarelo
 
-//SCORE PLAYERS
-int countBlack = 0;
-int countWhite = 0;
+// //SCORE PLAYERS
+// int countBlack = 0;
+// int countWhite = 0;
 
 
 class Pong{
