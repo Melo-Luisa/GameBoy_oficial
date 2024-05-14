dae6ac91b4037647a5f6b9e6109d3e9f1160bf22
Luisa <luhcotmel@gmail.com>
2024-05-13 22:19:03 -0300

    problemas com a classe Juiz

--- src/games/Pong/main.cpp
+++ src/games/Pong/main.cpp
@@ -1,11 +1,9 @@
 #include <Arduino.h>
 #include <TFT_eSPI.h> 
 #include <SPI.h>
-// #include "pongConfig.h"
-// #include "bola/ball.h"
+
 #include "juiz/utils.h"
 #include "pongConfig.h"
-
 
 TFT_eSPI d = TFT_eSPI();
 TFT_eSprite ball = TFT_eSprite(&d);
@@ -13,9 +11,17 @@ TFT_eSprite barra_joy = TFT_eSprite(&d);
 TFT_eSprite barra_button = TFT_eSprite(&d);
 TFT_eSprite placar = TFT_eSprite(&d);
 
-// Crie um objeto da classe Juiz
-// Crie um objeto da classe Juiz
-Juiz juiz = Juiz(x, y, vx, vy, circleRadius, countBlack, countWhite);
+// Defina valores para as variáveis ​​que serão passadas para o construtor de Juiz
+int x = 60; // valor inicial de x
+int y = 50; // valor inicial de y
+int vx = 10; // valor inicial de vx
+int vy = 10; // valor inicial de vy
+int circleRadius = 10; // raio do círculo
+int countBlack = 0; // contador de pontos preto
+int countWhite = 0; // contador de pontos branco
+
+// Crie um objeto da classe Juiz com os valores definidos
+Juiz juiz( x,  y,  vx,  vy,  circleRadius,  countBlack,  countWhite);
 
 void setup(){
     //precisa de nada
@@ -26,13 +32,13 @@ void setup(){
 
 void loop(){
     //trazer main
-    juiz.draw_ball(TFT_eSprite &ball); // desenha bola
-    juiz.placar( TFT_eSprite &placar, int countBlack, int countWhite); // desenha placar
+    juiz.draw_ball(ball); // desenha bola
+    juiz.placar(placar, countBlack, countWhite); // desenha placar
     juiz.hit_esquerda(); // retorna valor se atingiu esq
     juiz.hit_direita(); // retorna valor se atingiu na dire
     juiz.atingir(); // verifica se atingiu
-    juiz.count(int circleRadius, int countWhite, int countBlack); //conta os pontos
-    juiz.draw_joy(TFT_eSprite &barra_joy);
-    juiz.draw_button(TFT_eSprite &barra_button);
+    juiz.count(circleRadius, countWhite, countBlack); //conta os pontos
+    juiz.draw_joy(barra_joy);
+    juiz.draw_button(barra_button);
 
 }
