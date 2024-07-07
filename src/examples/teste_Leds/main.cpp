458f2000ddc07b46bc472d20af4a61ac7efecfd6
João Ferreira <100808096+JoaoFerreira123@users.noreply.github.com>
2024-07-06 22:48:06 -0300

    test leds ok?

--- src/examples/teste_Leds/main.cpp
+++ src/examples/teste_Leds/main.cpp
@@ -2,7 +2,7 @@
 #include<FastLED.h> // header file
 
 #define NUM_LEDS 60 // number of led present in your strip
-#define LED_PIN 33 // digital pin of your arduino
+#define LED_PIN 23 // digital pin of your arduino
 
 CRGB leds[NUM_LEDS];
 
@@ -13,7 +13,7 @@ void setup() {
 
 void loop() {
 
-leds[0] = CRGB::Blue; //glow 1st led as green
+leds[2] = CRGB::Green; //glow 1st led as green
 
 FastLED.show(); // apply the function on led strip
 
